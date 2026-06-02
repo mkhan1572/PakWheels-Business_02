148f01584e2418370fdeed8da2dbf0f86a027022
MUDDASSIR KHAN <https://github.com/mkhan1572/student-faculty-system.git>
2026-06-02 10:59:34 +0500

    Add database/sql_files/01_create_database.sql

---
+++ database/sql_files/01_create_database.sql
@@ -0,0 +1,10 @@
+-- ================================================================
+-- FILE         : 01_create_database.sql
+-- PROJECT      : Vehicle Market Analytics System
+-- DATABASE     : pakwheels_db
+-- DESCRIPTION  : Creates the database
+-- RUN ORDER    : Step 1  (run this FIRST)
+-- ================================================================
+
+CREATE DATABASE IF NOT EXISTS pakwheels_db;
+USE pakwheels_db;
